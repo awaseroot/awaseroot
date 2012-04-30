c6c801c89eddaa45ad78b6945b00f5daafd9cfb8
Henri Siponen <henri.siponen@gmail.com>
2012-04-30 18:13:30 +0300

    added descriptions

--- fabric/release_upgrade/fabfile.py
+++ fabric/release_upgrade/fabfile.py
@@ -7,8 +7,10 @@ env.warn_only=True
 env.paralle=True
 
 def r_upgrade_y():
+	"""Answer YES to all upgrade questions"""
 	sudo('echo "y\n\ny\ny\ny\ny\ny\ny\ny\ny\ny\ny\ny\ny\n" | DEBIAN_FRONTEND=noninteractive /usr/bin/do-release-upgrade')
 
 def r_upgrade_n():
+	"""Answer NO to (almost) all upgrade questions"""
 	sudo('echo "y\n\ny\nN\nN\nN\nN\nN\nN\nN\nN\nN\nN\nN\n" | DEBIAN_FRONTEND=noninteractive /usr/bin/do-release-upgrade')
 
