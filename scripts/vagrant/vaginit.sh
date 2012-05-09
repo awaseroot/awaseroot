7864fb789f9afbecbad59eefd9edb0f7ec1d7733
Henri Siponen <henri.siponen@gmail.com>
2012-05-09 07:55:32 +0300

    added avahi-daemon restart

--- scripts/vagrant/vaginit.sh
+++ scripts/vagrant/vaginit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # This script installs Vagrant, downloads a base box, configures it
-# to use IP 10.10.10.10 so that we can ssh to it. Then installs libnss-mdns
+# to use IP 10.10.10.10. Then installs libnss-mdns
 # to the vagrant box and packages it.
 # Run vagmulti.sh script after this to add multiple boxes.
 
@@ -12,6 +12,5 @@ vagrant init
 cat Vagrantfile | sed s/'# config.vm.network :hostonly, "192.168.33.10"'/'config.vm.network :hostonly, "10.10.10.10"'/ > tmpvag
 mv tmpvag Vagrantfile
 vagrant up
-ssh vagrant@10.10.10.10 sudo apt-get update
-ssh vagrant@10.10.10.10 sudo apt-get -y install libnss-mdns
+ssh vagrant@10.10.10.10 sh -c 'sudo apt-get update ; sudo apt-get -y install libnss-mdns ; sudo service avahi-daemon restart'
 vagrant package
