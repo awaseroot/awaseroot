fdcadb6cdf1abd7494f8300841e351ef13602820
Henri Siponen <henri.siponen@gmail.com>
2012-11-25 11:57:04 +0200

    new node definition for updated nagios3 module

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -1,5 +1,23 @@
 node default { include nagios_nrpe }
 
-node 'hngu.elisa' { include nagios3 }
-
 node 'thor.elisa' { include lamp }
+
+node 'win7box.local' { include nscp }
+
+node 'hngu.elisa' { 
+  class { 'nagios3':
+    version => 'latest',
+  }
+  linuxhost { 'linux1':
+    name    => 'Ubuntu Linux',
+    address => '192.168.100.111',
+  }
+  linuxhost { 'linux2':
+    name    => 'Xubuntu Linux',
+    address => 'thor.local'
+  }
+  winhost { 'win1':
+    name    => 'windows',
+    address => '192.168.100.25',
+  }
+}
