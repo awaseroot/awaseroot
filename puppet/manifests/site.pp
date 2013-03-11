13279ee55abb7858b2f63eaf1309ff71a932ed18
Henri Siponen <hng.siponen@gmail.com>
2013-03-11 19:19:39 +0200

    added service definitions

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -30,11 +30,32 @@ node 'hngu.elisa' {
     group   => 'group1',
     address => 'host1.local',
   }
+
   mygroup { 'group1': }
 
   myservice { 'checkfile':
     host => 'win1',
     cmd  => 'check_nrpe!check_files!/path:"c:\\temp" /namefilter:"test\.txt" /expectmatch:1 /age:30n /selage:newer /warning:1: /critical:1:',
     desc => 'File backup test',
+  }
+  myservice { 'memorycheck':
+    group => 'group1',
+    cmd   => 'check_nrpe!CheckMEM!MaxWarn=95% MaxCrit=98% ShowAll=long type=physical',
+    desc  => 'Memory usage',
+  }
+  myservice { 'drivecheck_c':
+    group => 'group1',
+    cmd   => 'check_nt!USEDDISKSPACE!-l c -w 90 -c 95',
+    desc  => 'C:\ Drive Space',
+  }
+  myservice { 'cpucheck':
+    group => 'group1',
+    cmd   => 'check_nrpe!CheckCPU!MaxWarn=80 MaxCrit=90 time=20m time=10s time=4',
+    desc  => 'CPU load',
+  }
+  myservice { 'cpucheck_linux':
+    host => 'linux1',
+    cmd  => 'check_nrpe_1arg!check_load',
+    desc => 'CPU load',
   }
 }
